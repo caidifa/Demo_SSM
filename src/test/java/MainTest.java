13943e1fd668fb3b66a35ec5d32b37beeacc09a1
caibaolong <caidifa@126.com>
2019-05-31 17:15:49 +0800

    添加一个测试类

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * Created by caibaolong on 2019/5/31.
+ */
+public class MainTest {
+    @Test
+    public void userTest() {
+
+        System.out.println("test me!!!");
+
+
+
+
+    }
+}
